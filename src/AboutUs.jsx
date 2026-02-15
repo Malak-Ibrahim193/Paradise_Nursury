d6a94383718bde4f9922d24d14ec8320c0ed0b26
Malak-Ibrahim193 <malakibrahim193@gmail.com>
2026-02-15 03:46:18 +0200

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is a leading online plant shop, offering a variety of houseplants
+        to beautify your home and office. We are passionate about greenery and sustainability.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
